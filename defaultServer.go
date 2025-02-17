9aa859d6b644eea09d72aa6886c4e94c271a9c58
bxdn <33440510+bxdn@users.noreply.github.com>
2025-02-17 17:25:46 -0500

    Made even more singleton-y

--- defaultServer.go
+++ defaultServer.go
@@ -12,14 +12,12 @@ type Endpoint interface {
 	Handler() RequestHandler
 }
 
-func (ds *defaultServer) Register(endpoints ...Endpoint) {
+func Register(endpoints ...Endpoint) {
 	for _, endpoint := range endpoints {
 		http.DefaultServeMux.HandleFunc(endpoint.Path(), endpoint.Handler())
 	}
 }
 
-func (ds *defaultServer) Serve(port int) error {
+func Serve(port int) error {
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), http.DefaultServeMux)
 }
-
-var DefaultServer = &defaultServer{}
