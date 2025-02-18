6eca3b3033399e8294d44b18c62203d37865f095
bxdn <33440510+bxdn@users.noreply.github.com>
2025-02-18 08:29:05 -0500

    unfunctionalized json middleware

--- middleware.go
+++ middleware.go
@@ -19,15 +19,13 @@ func Deserialize[T any](ctx context.Context) (*T, error) {
 	return typed, nil
 }
 
-func Json[T any]() RequestNode {
-	return func(w http.ResponseWriter, r *http.Request) (context.Context, error) {
-		ptr := new(T)
-		decoder := json.NewDecoder(r.Body)
-		if e := decoder.Decode(ptr); e != nil {
-			log.Println(e)
-			http.Error(w, "bad request", http.StatusBadRequest)
-			return r.Context(), e
-		}
-		return context.WithValue(r.Context(), jsonKey{}, ptr), nil
+func Json[T any](w http.ResponseWriter, r *http.Request) (context.Context, error) {
+	ptr := new(T)
+	decoder := json.NewDecoder(r.Body)
+	if e := decoder.Decode(ptr); e != nil {
+		log.Println(e)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return r.Context(), e
 	}
+	return context.WithValue(r.Context(), jsonKey{}, ptr), nil
 }
