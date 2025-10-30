8cd7a173f290a232b9dd8288f9f3deb0ec940d1e
bxdn <33440510+bxdn@users.noreply.github.com>
2025-10-30 12:54:09 -0400

    Fixed Json middleware?

--- middleware.go
+++ middleware.go
@@ -17,15 +17,17 @@ func (f *Flite[T]) Body() *T {
 	return typed
 }
 
-func (f *Flite[T]) Json() error {
-	ptr := new(T)
-	decoder := json.NewDecoder(f.req.Body)
-	if e := decoder.Decode(ptr); e != nil {
-		if e2 := f.ReturnError("Body is not in the correct JSON schema", http.StatusBadRequest); e2 != nil {
-			return fmt.Errorf("Error returning bad request error: %v", e2)
+func Json[T any](e Endpoint[T]) RequestNode[T] {
+	return func(f *Flite[T]) error {
+		ptr := new(T)
+		decoder := json.NewDecoder(f.req.Body)
+		if e := decoder.Decode(ptr); e != nil {
+			if e2 := f.ReturnError("Body is not in the correct JSON schema", http.StatusBadRequest); e2 != nil {
+				return fmt.Errorf("Error returning bad request error: %v", e2)
+			}
+			return nil
 		}
+		f.AddContext(jsonKey{}, ptr)
 		return nil
 	}
-	f.AddContext(jsonKey{}, ptr)
-	return nil
 }
