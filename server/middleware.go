0c2ace868ef18a8b1ec63042d14acd49e33bd128
bxdn <33440510+bxdn@users.noreply.github.com>
2025-10-31 14:58:27 -0400

    Fixed cors

--- server/middleware.go
+++ server/middleware.go
@@ -3,12 +3,12 @@ package server
 import "net/http"
 
 func Cors(w http.ResponseWriter, r *http.Request) (error, bool) {
-	if r.Method != http.MethodOptions {
-		return nil, false
-	}
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, PATCH, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.WriteHeader(http.StatusNoContent)
-	return nil, true
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
+		return nil, true
+	}
+	return nil, false
 }
