681c1c846a8bf37c60ee3da48e091468eecbac55
bxdn <33440510+bxdn@users.noreply.github.com>
2025-11-01 07:51:22 -0400

    Utility methods for websockets

--- server/websockets.go
+++ server/websockets.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
 )
 
 type wsep struct {
@@ -15,20 +17,56 @@ type wsep struct {
 
 func (w *wsep) Path() string { return w.path }
 
-func (w *wsep) Handler() func(http.ResponseWriter, *http.Request) { return w.handler }
+func (w *wsep) Handler() func(w http.ResponseWriter, r *http.Request) { return w.handler }
 
-func WS(path string, handler func(*websocket.Conn)) {
-	wrapper := func(w http.ResponseWriter, r *http.Request) {
+func createHandler(handler func(c *websocket.Conn) error) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		c, e := websocket.Accept(w, r, &websocket.AcceptOptions{InsecureSkipVerify: true})
 		defer c.CloseNow()
 		if e != nil {
 			log.Printf("ERROR accepting websocket connection: %v\n", e)
 			return
 		}
-		handler(c)
+		if e := handler(c); e != nil {
+			log.Printf("ERROR executing websocket logic: %v\n", e)
+		}
 	}
+}
+
+func WS[T any](path string, handler func(c *websocket.Conn) error) {
 	defaultServer.endpoints = append(defaultServer.endpoints, &wsep{
 		path:    fmt.Sprintf("GET %s", path),
-		handler: wrapper,
+		handler: createHandler(handler),
 	})
+}
+
+func ReadText(c *websocket.Conn) (string, error) {
+	_, msgBytes, e := c.Read(context.Background())
+	if e != nil {
+		return "", fmt.Errorf("Error Reading text from websocket: %w", e)
+	}
+	return string(msgBytes), nil
+}
+
+func ReadObj[T any](c *websocket.Conn) (T, error) {
+	ptr := new(T)
+	if e := wsjson.Read(context.Background(), c, ptr); e != nil {
+		return *ptr, fmt.Errorf("Error Reading obj from websocket: %w", e)
+	}
+	return *ptr, nil
+}
+
+func SendText(c *websocket.Conn, s string) error {
+	e := c.Write(context.Background(), websocket.MessageText, []byte(s))
+	if e != nil {
+		return fmt.Errorf("Error Sending text to websocket: %w", e)
+	}
+	return nil
+}
+
+func SendObj(c *websocket.Conn, obj any) error {
+	if e := wsjson.Write(context.Background(), c, obj); e != nil {
+		return fmt.Errorf("Error Sending object to websocket: %w", e)
+	}
+	return nil
 }
